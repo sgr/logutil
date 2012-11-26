ac720af82bc7ec3e1456ce8e53e8ec310bcd8e4c
Shigeru Fujiwara <sgr0502@gmail.com>
2012-11-26 23:03:08 +0900

    Enabled initializing LazyFileHandler with property file

--- src/handler.clj
+++ src/handler.clj
@@ -1,35 +1,57 @@
 ;; -*- coding: utf-8-unix -*-
 (ns handler
   (:import [java.io File FileOutputStream]
-           [java.util.logging Formatter LogRecord]))
+           [java.util.logging Formatter Level LogManager LogRecord SimpleFormatter]))
 
 (gen-class
  :name logutil.LazyFileHandler
  :extends java.util.logging.StreamHandler
- :constructors {[String java.util.logging.Formatter] []}
+ :constructors {[String java.util.logging.Formatter] []
+                [] []}
  :exposes-methods {publish publishSuper
                    setFormatter setFormatterSuper
+                   setLevel setLevelSuper
                    setOutputStream setOutputStreamSuper}
  :state state
  :init init
  :post-init post-init
+ :methods [[setPath [String] void]]
  :prefix "lfh-")
 
-(defn- lfh-init [^String path ^Formatter formatter]
-  (let [f (File. path)]
-    (if (.exists f)
-      [[] (atom {:file f :os (FileOutputStream. f true)})]
-      [[] (atom {:file f :os nil})])))
+(defn- lfh-init
+  ([]
+     [[] (atom {:file nil :opened false})])
+  ([^String path ^Formatter formatter]
+     [[] (atom {:file (File. path) :opened false})]))
 
-(defn- lfh-post-init [this ^String path ^Formatter formatter]
-  (.setFormatterSuper this formatter)
-  (when-let [os (:os @(.state this))]
-    (.setOutputStreamSuper this os)))
+(defn- lfh-setPath [this ^String path]
+  (swap! (.state this) assoc :file (File. path)))
+
+(defn- lfh-setFormatter [this ^Formatter formatter]
+  (.setFormatterSuper this (if formatter formatter (SimpleFormatter.))))
+
+(defn- lfh-init-with-prop [this]
+  (let [lmgr (LogManager/getLogManager)
+        path (.getProperty lmgr "logutil.LazyFileHandler.path")
+        fmtr (.getProperty lmgr "logutil.LazyFileHandler.formatter")]
+    (when path
+      (lfh-setPath this path))
+    (.setFormatterSuper this (if fmtr
+                               (.newInstance (Class/forName fmtr))
+                               (SimpleFormatter.)))))
+
+(defn- lfh-post-init
+  ([this]
+     (.setLevelSuper this Level/ALL)
+     (lfh-init-with-prop this))
+  ([this ^String path ^Formatter formatter]
+     (.setLevelSuper this Level/ALL)
+     (lfh-setFormatter this formatter)))
 
 (defn- lfh-publish [this ^LogRecord r]
-  (if-let [os (:os @(.state this))]
-    (.publishSuper this r)
-    (let [os (FileOutputStream. (:file @(.state this)) true)]
-      (swap! (.state this) assoc :os os)
-      (.setOutputStreamSuper this os)
-      (recur this r))))
+  (when-not (:opened @(.state this))
+    (lfh-init-with-prop this)
+    (let [f (:file @(.state this))]
+      (.setOutputStreamSuper this (FileOutputStream. f (.exists f))))
+    (swap! (.state this) assoc :opened true))
+  (.publishSuper this r))
