4086f36b39ad9b501624034151d9d08edaacb84f
Shigeru Fujiwara <sgr0502@gmail.com>
2013-03-07 02:16:10 +0900

    updated dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject logutil "0.2.0"
+(defproject logutil "0.2.1"
   :description "A Clojure library designed to use Java Logging API easily."
   :url "https://github.com/sgr/logutil"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main logutil
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.logging "0.2.4"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/tools.logging "[0.2,)"]])
