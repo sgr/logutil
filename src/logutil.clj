1b864ffe441b63f6cf853f5fd6fcab9ec17aa6cb
Shigeru Fujiwara <sgr0502@gmail.com>
2013-05-16 22:30:15 +0900

    added example to comment

--- src/logutil.clj
+++ src/logutil.clj
@@ -16,7 +16,15 @@
     (ByteArrayInputStream. (.toByteArray baos))))
 
 (defn configure-logging
-  "Reinitialize logging configuration from a property map."
+  "Reinitialize logging configuration from a property map:
+
+     {\"handlers\" \"java.util.logging.MemoryHandler\"
+      \"java.util.logging.MemoryHandler.target\" \"logutil.LazyFileHandler\"
+      \"logutil.LazyFileHandler.path\" \"tmp.log\"
+      \"logutil.LazyFileHandler.level\" \"ALL\"
+      \"logutil.LazyFileHandler.formatter\" \"logutil.Log4JLikeFormatter\"
+      \".level\" \"INFO\"
+      \"example.level\" \"ALL\"}"
   [m]
   (doto (LogManager/getLogManager)
     (.readConfiguration (config-stream m))))
